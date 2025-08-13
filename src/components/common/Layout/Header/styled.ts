1418da5252e00ed599bd360d202ffe2190922902
Linn-Devoteam <linn.johansson@devoteam.com>
2025-08-13 14:31:06 +0200

    Add header to App.tsx and update correct bg color and responsive Heading3

---
+++ src/components/common/Layout/Header/styled.ts
@@ -0,0 +1,28 @@
+import styled from '@emotion/styled'
+import tokens from '../../../../core/tokens'
+
+export const Header = styled.header`
+  position: sticky;
+  top: 0;
+  background: ${({ theme }) => theme.colors.white};
+  border-bottom: ${tokens.size.BASELINE / 8}px solid
+    ${({ theme }) => theme.colors.scrollBar};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: ${tokens.padding.CONTAINER}px;
+
+  & > div {
+    display: flex;
+    flex-direction: column;
+    gap: ${tokens.size.BASELINE}px;
+  }
+
+  @media (max-width: ${tokens.breakpoints.phone}) {
+    padding: ${tokens.padding.BASELINE * 2}px;
+
+    & > div {
+      gap: ${tokens.size.BASELINE / 2}px;
+    }
+  }
+`
