8e7ebd2dc417a4286d61824d10d007faaf5678df
Linn-Devoteam <linn.johansson@devoteam.com>
2025-08-13 11:32:56 +0200

    Add reusable Section wrapper for showing tabs information + shared BaseContainer & DividerBar

---
+++ src/components/common/foundation/Section/Section.tsx
@@ -0,0 +1,23 @@
+import type { ReactNode } from 'react'
+import * as S from './styled'
+import { Typography as T } from '../../../../core/typography'
+import { DividerBar } from '../../styled'
+
+type Props = {
+  title: string
+  children: ReactNode
+}
+
+export default function Section({ title, children }: Props) {
+  return (
+    <S.Wrapper>
+      <S.Header>
+        <T.BodyMedium demi margin="0">
+          {title}
+        </T.BodyMedium>
+      </S.Header>
+      <DividerBar />
+      <S.Body>{children}</S.Body>
+    </S.Wrapper>
+  )
+}
