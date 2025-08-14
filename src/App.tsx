b8b4268a2bf071ebd8611671db8300b60f7c7f64
Linn-Devoteam <linn.johansson@devoteam.com>
2025-08-14 10:44:07 +0200

    Refactor App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,12 @@ import { Typography as T } from './core/typography'
 function App() {
   return (
     <>
-      <div
-        style={{
-          background: 'white',
-          padding: 30,
-          gap: 10,
-          display: 'flex',
-          flexDirection: 'column',
-        }}
-      >
-        <T.Heading3 style={{ margin: 0 }}>
-          Welcome to React Vite Template
-        </T.Heading3>
-        <T.BodyBase style={{ margin: 0 }}>
-          This is a simple template to get you started with React and Vite.
-        </T.BodyBase>
-      </div>
+      <T.Heading3 style={{ margin: '20px' }}>
+        Welcome to React Vite Template
+      </T.Heading3>
+      <T.BodyBase style={{ margin: '20px' }}>
+        This is a simple template to get you started with React and Vite.
+      </T.BodyBase>
 
       <Main>
         <Tabs
