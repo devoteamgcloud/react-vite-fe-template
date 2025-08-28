fe29b5902e80810b7d5f16990354c5f04ce99b63
Linn-Devoteam <linn.johansson@devoteam.com>
2025-08-28 12:31:30 +0200

    Make navbar sticky under header when scroll. Add useElementHeight Hook to get Header height

--- src/pages/LiberyPage/index.tsx
+++ src/pages/LiberyPage/index.tsx
@@ -2,17 +2,21 @@ import Tabs from '../../components/common/organisms/Tabs'
 import { PaddedContainer } from '../../components/common/styled'
 import { LIBRARY_TABS } from './tabs.config'
 import Header from '../../components/common/Layout/Header'
+import { useElementHeight } from '../../hooks/useElementHeight'
 
 export default function LibraryPage() {
+  const { ref, height } = useElementHeight<HTMLDivElement>()
+
   return (
     <>
       <Header
         title="React-Vite Template"
         subtitle="Explore reusable components, hooks, and patterns"
+        forwardedRef={ref}
       />
 
       <PaddedContainer>
-        <Tabs items={LIBRARY_TABS} defaultKey="overview" />
+        <Tabs offset={height} items={LIBRARY_TABS} defaultKey="overview" />
       </PaddedContainer>
     </>
   )
